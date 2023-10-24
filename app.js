b5e756c679c9c76e71747c4fda04bf0238d7388b
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-24 09:22:55 +0000

    [Add] 条件分岐

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-let hello = 'Hello World';
-alert('Hello' + 'World');
+//let hello = 'Hello World';
+//alert('Hello' + 'World');
 
 //それぞれの文字列を変数に格納して結合
-let str1 = 'Hello';
-let str2 = 'World!!';
-alert(str1 + str2)
+//let str1 = 'Hello';
+//let str2 = 'World!!';
+//alert(str1 + str2)
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がリンゴより安い')
+} else if(orange == apple){
+  alert('みかんとリンゴが同じ値段')
+} else{
+  alert('みかんの値段がリンゴより高い')
+}
