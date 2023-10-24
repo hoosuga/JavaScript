3363c50f67cbee98eb3e33651783923aebda48e9
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-24 09:52:29 +0000

    [Add] while文

--- app.js
+++ app.js
@@ -6,13 +6,24 @@
 //let str2 = 'World!!';
 //alert(str1 + str2)
 
-let orange = 100;
-let apple = 120;
+//let orange = 100;
+//let apple = 120;
 
-if(orange < apple){
-  alert('みかんの値段がリンゴより安い')
-} else if(orange == apple){
-  alert('みかんとリンゴが同じ値段')
-} else{
-  alert('みかんの値段がリンゴより高い')
+//if(orange < apple){
+//  alert('みかんの値段がリンゴより安い')
+//} else if(orange == apple){
+//  alert('みかんとリンゴが同じ値段')
+//} else{
+//  alert('みかんの値段がリンゴより高い')
+//}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
 }
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
