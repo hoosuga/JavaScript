4636537568081a5b1c7796b2792d53f9b1ad91bb
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-25 06:00:50 +0000

    [Add] 演習

--- app.js
+++ app.js
@@ -28,11 +28,18 @@
 
 //alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-let i;
-let num = 0;
+//let i;
+//let num = 0;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
+//for(i = 1; i < 11; i++){
+//  num = num + i;
+//}
 
-alert('1から10まで足した計算結果は' + num + 'です')
+//alert('1から10まで足した計算結果は' + num + 'です')
+
+let str1 = '1+3='
+let str2 = 1+3;
+let str3 = '5-3='
+let str4 = 5-3;
+alert(str1 + str2); 
+alert(str3 + str4);
