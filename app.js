a9f742ed311434a67bc0847643868eb041c8af55
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-26 10:01:08 +0000

    [Update] 入力ダイアログで値を入力

--- app.js
+++ app.js
@@ -55,5 +55,10 @@
 //  return addStr;
 //}
 
-let promptStr = prompt('何か好きな文字を入力してください。')
-alert(promptStr)
+//let promptStr = prompt('何か好きな文字を入力してください。')
+//alert(promptStr)
+
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');
