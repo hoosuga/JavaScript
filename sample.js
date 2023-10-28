0518a3a9b8249fde36984e6e047b40c24d37fba9
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-10-28 09:54:37 +0000

    [Add] ホイスティング

--- sample.js
+++ sample.js
@@ -1,5 +1,15 @@
-for (let i = 0; i < 10; i++) {     
-  console.log(i) 
+//for (let i = 0; i < 10; i++) {     
+//  console.log(i) 
+//}
+
+//console.log(i)
+
+var str = "webcamp"
+
+function foo(){
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
 }
 
-console.log(i)
+foo()
